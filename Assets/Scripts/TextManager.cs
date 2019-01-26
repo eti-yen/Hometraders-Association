3090127a3ce9914468e18080d686a10a3c78b767
TonyWang98 <wqtls1998@gmail.com>
2019-01-26 08:53:55 -0500

    Set up some UI

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextManager : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    public Camera mainCamera;
+    public int restartSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("here");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            print(hit.collider.name);
+        }
+        else
+        {
+
+        }
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(restartSceneIndex);
+    }
+}
